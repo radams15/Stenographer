7909b33a7b43196bcd5ab22b5127357014dd0015
rhys <email.curly@gmail.com>
2019-04-22 15:41:54 +0100

    Added Usage Message If No Args Supplied

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from sys import argv as args
 
 testing = False
 
+usage="""
+Usage:
+main.py hide [DATA] [IN_FILE] [OUT_FILE]
+
+main.py unhide [IN_FILE]
+"""
+
 if __name__ == '__main__':
     if testing:
         basic_stenographer = BasicStenographer(testing=True)
@@ -25,16 +32,22 @@ if __name__ == '__main__':
     else:
         basic_stenographer = BasicStenographer()
 
-        op_type = args[1]
-        if op_type == "hide":
-            data = args[2]
-            in_file = args[3]
-            out_file = args[4]
+        try:
+            op_type = args[1].lower()
+            if op_type == "hide":
+                data = args[2]
+                in_file = args[3]
+                out_file = args[4]
 
-            basic_stenographer.hide(data, in_file, out_file)
+                basic_stenographer.hide(data, in_file, out_file)
 
-        elif op_type == "unhide":
-            in_file = args[2]
+            elif op_type == "unhide":
+                in_file = args[2]
 
-            data = basic_stenographer.unhide(in_file)
-            print(f"Data:\n{data}")
+                data = basic_stenographer.unhide(in_file)
+                print(f"Data:\n{data}")
+
+            else:
+                raise Exception() # throw exception to print usage
+        except:
+            print(usage)
